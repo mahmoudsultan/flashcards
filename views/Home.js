d8f3f119a1d828cb92181c5f2860bec7c05f31db
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2021-02-25 09:17:30 +0200

    Initial View Commit.

---
+++ views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (<View>
+    <Text>Hello, World</Text>
+  </View>);
+};
+
+export default Home;
